0027087ed12b4ebbc267897b98642763cacb4858
Ethan Kent <ekent@jd11.law.harvard.edu>
2018-08-30 08:27:00 -0500

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Body from "./components/Body";
+import Navbar from "./components/Navbar";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="App container-fluid">
+        <Navbar />
+        <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+          <div className="row">
+            <Body />
+          </div>
+        </main>
       </div>
     );
   }
